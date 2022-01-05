8f0211c4a08bc594a639141cbd78963ea7652b10
hasa1497 <hamza.saeed@tu-ilmenau.de>
2022-01-05 19:00:32 +0100

    add dotnet server

---
+++ Demo/Program.cs
@@ -0,0 +1,40 @@
+using Demo.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services
+        .AddGraphQLServer()
+        .AddQueryType<Query>();
+
+builder.Services.AddCors();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+app
+    .UseRouting()
+    .UseCors(
+        options => options.WithOrigins("*").AllowAnyMethod()
+    )
+    .UseEndpoints(endpoints =>
+    {
+        endpoints.MapGraphQL();
+    });
+
+//app.UseAuthorization();
+
+//app.MapRazorPages();
+
+app.Run();
